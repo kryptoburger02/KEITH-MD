92bf94125aac45c30d848924a57ae680744c11ce
kryptoburger02 <kryptoburger03@gmail.com>
2025-04-07 09:03:01 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,9 +4,9 @@ const session = process.env.SESSION || '';
 
 const prefix = process.env.PREFIX || '';
 const mycode = process.env.CODE || "254";
-const author = process.env.OWNER_NAME || 'Keith';
-const packname = process.env.PACKNAME || 'keith';
-const dev = process.env.OWNER_NUMBER || '254748387615';
+const author = process.env.OWNER_NAME || 'Keith bot';
+const packname = process.env.PACKNAME || 'keith bot';
+const dev = process.env.OWNER_NUMBER || '+2347044290829';
 const DevKeith = dev.split(",");
 const botname = process.env.BOTNAME || 'KEITH-MD';
 const mode = process.env.MODE || 'public';
